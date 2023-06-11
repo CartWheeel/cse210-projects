c2baae38861db44fa9a76e945b29700a75db9a2b
CartWheeel <carterhwms@gmail.com>
2023-06-10 18:29:41 -0600

    this program prompts a user to guess a number and will give answers in a loop until they guess it right.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int answer = 0;
+
+        // We could also use a do-while loop here...
+        while (answer != magicNumber)
+        {
+            Console.Write("What is your answer? ");
+            answer = int.Parse(Console.ReadLine());
+
+            if (magicNumber > answer)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < answer)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+            // to test
+            // Console.WriteLine($"{magicNumber}");
+
+        }                    
     }
 }
