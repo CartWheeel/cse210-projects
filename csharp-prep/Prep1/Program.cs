cb488c645e4b4ffc870ae96b10065f456fa2d5d5
CartWheeel <carterhwms@gmail.com>
2023-06-10 18:07:04 -0600

    prep 1 assignment to ask for user's first and last name and output

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // What is your first name? Scott
+        // What is your last name? Burton
+        // Your name is Burton, Scott Burton.
+        //
+        //get user first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        //get user last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        //output user name in sentence
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
