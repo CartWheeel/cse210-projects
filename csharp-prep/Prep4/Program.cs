73042b54314f7587984f2e1159fbe01a74e3600a
CartWheeel <carterhwms@gmail.com>
2023-06-10 18:36:39 -0600

    this program loops over asking the user for a number and then will output the sum, average, and the largest number

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int num;
+        do
+        {
+            Console.Write("Enter any number (0 to quit): ");
+            string userResponse = Console.ReadLine();
+            num = int.Parse(userResponse);
+
+            if (num != 0)
+            {
+                numbers.Add(num);
+            }
+        } while (num != 0);
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
